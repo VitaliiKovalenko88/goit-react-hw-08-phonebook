3b6758fad1178b10fafb4bb8afe4907ba9196729
VitaliiKovalenko88 <75671142+VitaliiKovalenko88@users.noreply.github.com>
2024-01-02 19:08:43 +0100

    delete import

--- src/components/AppBar.jsx/AppBar.jsx
+++ src/components/AppBar.jsx/AppBar.jsx
@@ -2,7 +2,6 @@ import { AuthNav } from 'components/AuthNav/AuthNav';
 import { Navigation } from 'components/Navigation/Navigation';
 import { UserMenu } from 'components/UserMenu/UserMenu';
 import { useAuth } from 'hooks/useAuth';
-import React from 'react';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
